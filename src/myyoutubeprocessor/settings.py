ea44fd595be92ea271f71d502bafb0f94f925199
Gustave <muhozag@gmail.com>
2025-04-09 10:08:34 -0400

    updated markdown rendering

--- src/myyoutubeprocessor/settings.py
+++ src/myyoutubeprocessor/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'videos',  # Custom app for video processing
+    'markdown_deux',  # For markdown rendering
 ]
 
 MIDDLEWARE = [
